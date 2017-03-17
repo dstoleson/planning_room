ed84d617b9eb3495a5a07f6a43d60be85edfc272
David Stoleson <dstoleson@mac.com>
2017-03-17 10:22:38 -0500

    Revert archive project feature - forgot to add projects_controller to commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,20 +6,15 @@ class ProjectsController < ApplicationController
 	def index
 		Rails.logger.debug 'DEBUG: enter index'
 		if admin_user?
-			show_archive_switch
-			if @show_archive			
-				@projects = Project.order(name: :asc)
-			else
-				@projects = Project.where(deleted: false).order(name: :asc)
-			end
+			@projects = Project.order(name: :asc)
 		else
 			reset_session
-			@projects = Project.joins(:project_type).where(project_types: {name: "open"}, deleted: false).order(name: :asc)
-		end		
+			@projects = Project.joins(:project_type).where(project_types: {name: "open"}).order(name: :asc)
+		end
 	end
 
 	def new
-		@project = Project.new		
+		@project = Project.new
 		@button_title = "Create Project"
 		@title = "New Project"
 	end
@@ -28,14 +23,14 @@ class ProjectsController < ApplicationController
 		Rails.logger.debug 'DEBUG: enter create'
 		@project = Project.new(project_params)
 		if @project.save
-			redirect_to projects_path, :notice => "Project: #{@project.name}, created."			
-		end			
+			redirect_to projects_path, :notice => "Project: #{@project.name}, created."
+		end
 		@button_title = "Create Project"
 		@title = "New Project"
 	end
 
 	def edit
-		@project = Project.find(params[:id])		
+		@project = Project.find(params[:id])
 		project_activities
 		@button_title = "Update Project"
 		@title = "Edit Project"
@@ -44,9 +39,9 @@ class ProjectsController < ApplicationController
 	def update
 		@project = Project.find(params[:id])
 		if @project.update(project_params)
-			redirect_to projects_path, :notice => "Project: #{@project.name}, updated."			
+			redirect_to projects_path, :notice => "Project: #{@project.name}, updated."
 			return
-		end				
+		end
 		project_activities
 
 		@button_title = "Update Project"
@@ -55,41 +50,18 @@ class ProjectsController < ApplicationController
 
 	def show
 		Rails.logger.debug 'DEBUG: enter show'
-		@project = Project.find(params[:id])		
+		@project = Project.find(params[:id])
 
 		if admin_user?
 			redirect_to edit_project_path @project
 		end
 	end
 
-	def archive
-		Rails.logger.debug 'DEBUG: enter archive'
-		@project = Project.find(params[:id])
-		@project.deleted = true
-		@project.save
-		redirect_to projects_path
-	end
-
-	def unarchive
-		Rails.logger.debug 'DEBUG: enter unarchive'
-		@project = Project.find(params[:id])
-		@project.deleted = false
-		@project.save
-		redirect_to projects_path
-	end
-
-	def switch_show_archive
-		Rails.logger.debug 'DEBUG: enter switch_show_archive'
-		show_archive = show_archive_switch
-		session[:show_archive] = (not show_archive)
-		redirect_to projects_path
-	end
-
 	private
 
 	def project_params
 		Rails.logger.debug("DEBUG: params = #{params}")
-    	params.require(:project).permit(:name, 
+    	params.require(:project).permit(:name,
     		:project_type,
     		:password,
     		:password_confirmation,
@@ -103,15 +75,6 @@ class ProjectsController < ApplicationController
   	def project_activities
 		@sorted_project_activities = @project.project_activities.order("to_char(created_at, 'YYYYMMDD') DESC", email: :asc)
 		@sorted_project_email = @project.project_activities.order(email: :asc)
-	end	
-
-	def show_archive_switch
-		if session[:show_archive].nil?
-			session[:show_archive] = false
-		end
-
-		@show_archive = session[:show_archive]
 	end
+
 end
-
-
